f323594aec3b4dfc7cf1d641d078ec571443a828
asmaretaayu <asmaretaayu@gmail.com>
2023-11-07 15:52:42 +0700

    tugas 6

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Author extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function book(): HasOne
+    {
+        return $this->hasOne(Book::class);
+    }
 }
